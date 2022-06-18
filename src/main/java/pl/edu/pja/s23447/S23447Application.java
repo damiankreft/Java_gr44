7c5db2a7b4bd51fa92c55ac9de9eddceee370ce7
Damian Kreft <damiankreft87@gmail.com>
2022-06-18 09:22:43 +0200

    Add project files.

---
+++ src/main/java/pl/edu/pja/s23447/S23447Application.java
@@ -0,0 +1,13 @@
+package pl.edu.pja.s23447;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S23447Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S23447Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/pja/s23447/S23447ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pja.s23447;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S23447ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
